ece788fe52c732f59157be7a2bc3d339a23398e5
sagor <sagorhossainnil.386@gmail.com>
2025-08-25 09:48:53 +0800

    Add transaction history feature

--- home.js
+++ home.js
@@ -1,5 +1,7 @@
 const validPin = 1234;
 
+const transactionData = [];
+
 // function to get input value
 function getInputValue(id) {
     return document.getElementById(id).value;
@@ -19,11 +21,6 @@ function getInnerText(id){
 function setInnerText(value){
     return parseInt(document.getElementById('available-balance').innerText = value);
 }
-
-// log out button interactivity
-document.getElementById('logout-btn').addEventListener('click', function (){
-    window.location.href = './index.html'
-})
 
 // function to toggle
 function handleToggle(id){
@@ -45,41 +42,10 @@ function handleToggleEffect(id){
     document.getElementById(id).classList.add('border-[#0874f2]', 'bg-[#0874f20d]', 'text-blue-500');
 }
 
-// add money button interativity 
-document.getElementById('add-money-btn').addEventListener('click', function(e){
-    e.preventDefault();
-
-    const bankName = getInputValue('bank-name');
-    const accountNumber = getInputValue('account-number');
-    const addAmount = getInputValueNumber('add-amount');
-    const pinNumber = getInputValueNumber('add-pin');
-
-    if (accountNumber.length < 11 || accountNumber.length > 11) {
-        alert('Please provide valid account number');
-        return;
-    }
-
-    if (pinNumber !== validPin) {
-        alert('Please provide valid pin');
-        return;
-    }
-
-    if (addAmount <= 0) {
-        alert('Please provide valid amount');
-        return;
-    }
-
-    const availableBalance = getInnerText('available-balance');
-    const newAvailableBalance = availableBalance + addAmount;
-    setInnerText(newAvailableBalance);
-
-    document.getElementById('account-number').value = '';
-    document.getElementById('add-amount').value = '';
-    document.getElementById('add-pin').value = '';
-    document.getElementById('bank-name').value = document.getElementById('select-bank').innerText;
-
-    alert(`$${addAmount} Add money successful. Balance: $${newAvailableBalance}`);
-});
+// function to make input field empty after transactions
+function emptyStr(id) {
+    document.getElementById(id).value = '';
+}
 
 // defaut display 
 document.getElementById('add-money-container').style.display = 'none';
@@ -87,7 +53,7 @@ document.getElementById('cashout-container').style.display = 'none';
 document.getElementById('transfer-money-container').style.display = 'none';
 document.getElementById('get-bonus-container').style.display = 'none';
 document.getElementById('pay-bill-container').style.display = 'none';
-document.getElementById('transaction-container').style.display = 'none';
+document.getElementById('transaction-container').style.display = 'block';
 
 // toggling feature 
 document.getElementById('add-money').addEventListener('click', function(){
@@ -117,14 +83,54 @@ document.getElementById('pay-bill').addEventListener('click', function(){
 
 document.getElementById('transaction').addEventListener('click', function(){
     handleToggle('transaction-container');
+    handleToggleEffect('transaction');
+});
 
-    const formBtns = document.getElementsByClassName('form-btn');
-    for (const btn of formBtns){
-        btn.classList.remove('border-[#0874f2]', 'bg-[#0874f20d]', 'text-blue-500');
-        btn.classList.add('border-gray-200');
+// log out button interactivity
+document.getElementById('logout-btn').addEventListener('click', function (){
+    window.location.href = './index.html'
+});
+
+// add money button interativity 
+document.getElementById('add-money-btn').addEventListener('click', function(e){
+    e.preventDefault();
+
+    const bankName = getInputValue('bank-name');
+    const accountNumber = getInputValue('account-number');
+    const addAmount = getInputValueNumber('add-amount');
+    const pinNumber = getInputValueNumber('add-pin');
+
+    if (accountNumber.length < 11 || accountNumber.length > 11) {
+        alert('Please provide valid account number');
+        return;
     }
-    document.getElementById('transaction').classList.remove('border-gray-200');
-    document.getElementById('transaction').classList.add('border-[#0874f2]', 'bg-[#0874f20d]', 'text-blue-500');
+
+    if (pinNumber !== validPin) {
+        alert('Please provide valid pin');
+        return;
+    }
+
+    if (addAmount <= 0) {
+        alert('Please provide valid amount');
+        return;
+    }
+
+    const availableBalance = getInnerText('available-balance');
+    const newAvailableBalance = availableBalance + addAmount;
+    setInnerText(newAvailableBalance);
+
+    emptyStr('account-number');
+    emptyStr('add-amount');
+    emptyStr('add-pin');
+    document.getElementById('bank-name').value = document.getElementById('select-bank').innerText;
+
+    alert(`$${addAmount} Add money successful. Balance: $${newAvailableBalance}`);
+
+    const data = {
+        name: 'Add Money',
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data);
 });
 
 // cashout button interactivity
@@ -153,11 +159,17 @@ document.getElementById('cashout-button').addEventListener('click', function(e){
     const newAvailableBalance = availableBalance - cashoutAmount;
     setInnerText(newAvailableBalance);
 
-    document.getElementById('agent-number').value = '';
-    document.getElementById('cashout-amount').value = '';
-    document.getElementById('cashout-pin').value = '';
+    emptyStr('agent-number');
+    emptyStr('cashout-amount');
+    emptyStr('cashout-pin');
 
     alert(`$${cashoutAmount} cash out successful. Balance: $${newAvailableBalance}`);
+
+    const data = {
+        name: 'Cashout',
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data);
 });
 
 // transfer money button interactivity
@@ -186,9 +198,40 @@ document.getElementById('transfer-button').addEventListener('click', function(e)
     const newAvailableBalance = availableBalance - transferAmount;
     setInnerText(newAvailableBalance);
 
-    document.getElementById('recipient-number').value = '';
-    document.getElementById('transfer-amount').value = '';
-    document.getElementById('transfer-pin').value = '';
+    emptyStr('recipient-number');
+    emptyStr('transfer-amount');
+    emptyStr('transfer-pin');
 
     alert(`$${transferAmount} transferred to ${recipientNumber} successful. Balance: $${newAvailableBalance}`);
+
+    const data = {
+        name: 'Add Money',
+        date: new Date().toLocaleTimeString()
+    }
+    transactionData.push(data);
 });
+
+// display transactionData 
+document.getElementById('transaction').addEventListener('click', function (){
+    const transactionHistory = document.getElementById('transaction-history-container');
+    transactionHistory.innerText = ''
+
+    for (const data of transactionData) {
+        const div = document.createElement('div');
+        div.innerHTML = `
+            <div class="flex justify-between items-center bg-white rounded-xl p-3 mt-3">
+                <div class="flex items-center gap-3">
+                    <div class="p-3 rounded-full bg-[#f4f5f7]">
+                        <img src="./assets/wallet1.png" alt="">
+                    </div>
+                    <div>
+                        <h2>${data.name}</h2>
+                        <p>${data.date}</p>
+                    </div>
+                </div>
+                <i class="fa-solid fa-ellipsis-vertical"></i>
+            </div>
+        `
+        transactionHistory.appendChild(div);
+    }
+})
